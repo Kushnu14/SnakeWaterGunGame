9cd3a5fc52f9d765d7d53fd69f0693448ee24425
Kushnu14 <152047235+Kushnu14@users.noreply.github.com>
2024-06-23 22:53:12 +0530

    Update SnakeGunWater_Game.py
    Commited

--- SnakeGunWater_Game.py
+++ SnakeGunWater_Game.py
@@ -13,26 +13,29 @@ while True:
     else:
        computer = random.choice([-1, 0, 1])
        youstr = input("Enter 's' for Snake, 'w' for Water, 'g' for Gun: ").strip().lower()
-       
-
-       youDict = {
+       if youstr!="s" and youstr!="w" and youstr!="g":
+           print("Enter valid alphabet")
+       else:
+        youDict = {
             "s": 1,
             "w": -1,
             "g": 0
-        }
-       reverseDict = {
+         }
+        reverseDict = {
             1: "Snake", -1: "Water", 0: "Gun"
         }
-       you = youDict[youstr]
-       print(f"You choose {reverseDict[you]}\nComputer choose {reverseDict[computer]}")
+        you = youDict[youstr]
+        print(f"You choose {reverseDict[you]}\nComputer choose {reverseDict[computer]}")
 
-       if computer == you:
+        if computer == you:
             print("It's a draw!")
-       else:
+        else:
             if (computer == 1 and you == -1) or (computer == 1 and you == 0) or (computer == 0 and you == -1):
                 print("You Lose!")
             elif (computer == 0 and you == 1) or (computer == -1 and you == 0) or (computer == -1 and you == 1):
                 print("You Win!")
 
+
+       
 
     
